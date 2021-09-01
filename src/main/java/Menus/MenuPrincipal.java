6b443f3445570a95526e026a37b688b6922a62b5
juuann04 <juanlasartefp@gmail.com>
2021-09-01 11:47:09 +0200

    0.2

---
+++ src/main/java/Menus/MenuPrincipal.java
@@ -0,0 +1,14 @@
+package Menus;
+
+public class MenuPrincipal {
+    public static void imprimirMenuPrincipal() {
+        System.out.println("1.- Listar ficheros y directorios");
+        System.out.println("2.- Subir fichero");
+        System.out.println("3.- Descargar fichero del servidor");
+        System.out.println("4.- Eliminar fichero");
+        System.out.println("5.- Eliminar directorio");
+        System.out.println("6.- Crear directorio");
+        System.out.println("7.- Cambiar el directorio actual");
+        System.out.println("8.- Establecer conexion remotamente");
+    }
+}
